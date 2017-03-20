edc4c2c3e0d3b4776ae50e85018a66a2c549c646
Mitchell Yuan <yuanshuai329@gmail.com>
2017-03-20 23:36:18 +1300

    SaleEasy project initialization

---
+++ SaleEasy/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  SaleEasy
+//
+//  Created by Mitchell Yuan on 20/03/17.
+//  Copyright © 2017 Mitchell Yuan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
